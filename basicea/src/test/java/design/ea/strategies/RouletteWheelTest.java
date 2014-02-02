b682e9f41ee4bdefd8a22fa42237bc6fc7546cf3
Jaroslav Vitku <jarda.vitku@gmail.com>
2014-02-02 01:48:41 +0100

    roulettewheel unit test ok

--- basicea/src/test/java/design/ea/strategies/RouletteWheelTest.java
+++ basicea/src/test/java/design/ea/strategies/RouletteWheelTest.java
@@ -1,57 +1,75 @@
 package design.ea.strategies;
 
-import java.util.Random;
+import static org.junit.Assert.*;
 
-import tools.utils.DU;
-import tools.utils.LU;
+import org.junit.Test;
 
-import design.ea.matrix.Population;
-import design.ea.matrix.ann.impl.Ind;
+import design.ea.algorithm.Population;
+import design.ea.algorithm.impl.SingleVectorPop;
+import design.ea.ind.fitness.simple.impl.RealValFitness;
 import design.ea.strategies.impl.RouletteWheel;
 
+/**
+ * Note: precondition of this test is the AbstractSelectionTest passed
+ * 
+ * @author Jaroslav Vitku
+ *
+ */
 public class RouletteWheelTest {
 
-	public static void main(String[] args){
-		RouletteWheelTest rt = new RouletteWheelTest();
-		rt.checkProbability();
-	}
-	
-	public void checkProbability(){
+	@Test
+	public void checkRoulette(){
 		// EA setup
-			int INdim = 10;
-			int OUTdim = 20;
-			int neurons=100;
-			float max = 100; float min = -100;
+		int len = 20;
+		float max = 100; float min = -100;
 
-			// pop setup
-			int num = 10;
-			Population p = new Population(num);
+		// pop setup
+		int num = 10;
+		Population p = new SingleVectorPop(num, len, min, max);
+
+		this.randomizeFitness(p);
+
+		// make many selections
+		RouletteWheel s = new RouletteWheel();
+		s.resetSelection(p);
+		int poc = 1000000;
+		
+		int[] numSelected = new int[p.size()];	// how many times is i selected?
+		for(int i=0; i<numSelected.length; i++)
+			numSelected[i] = 0;
+		
+		for(int i=0; i<=poc; i++){				// select someone poc-times
+			numSelected[s.select(1)[0]]++;
+		}
+		
+		// now sort the population according to the fitness
+		int [] sorted = s.sort();
+		
+		for(int i=0; i<p.size()-1; i++){			// for all sorted individuals
 			
-			for(int i=0; i<num;i++){
-				p.setInd(i,new Ind(INdim, OUTdim, neurons, max, min));
-			}
-			this.randomizeFitness(p);
+			// just re-check whether fitness is decreasing
+			assertFalse(
+					p.get(sorted[i+1]).getFitness().betterThan(
+					p.get(sorted[i]).getFitness()));
 			
-			// make selection
-			RouletteWheel s = new RouletteWheel();
-			s.resetSelection(p);
-			int poc = 100000;
-			// all selected zero times
-			int[] numSelected = new int[p.size()];
-			for(int i=0; i<numSelected.length; i++)
-				numSelected[i] = 0;
-			for(int i=0; i<=poc; i++){
-				numSelected[s.select(1)[0]]++;
-			}
-			DU.pl("inds with these fitness values: "+LU.toStr(p.getArrayOfFitnessVals()));
-			DU.pl(" were selected these times:     "+LU.toStr(numSelected));
+			// check that number of selections is decreasing too
+			System.out.println("Fitness vals: "+
+			((RealValFitness)p.get(sorted[i+1]).getFitness()).getFitness()+" <= "+
+			((RealValFitness)p.get(sorted[i  ]).getFitness()).getFitness()+
+			"\tnum selections: "+numSelected[sorted[i+1]] +" <= "+
+			numSelected[sorted[i]]);
+			
+			assertTrue(
+					numSelected[sorted[i+1]] <= numSelected[sorted[i]]);
+			
+		}
 	}
-	
+
 	private void randomizeFitness(Population pop){
-		int k = 1;
-		Random r = new Random();
 		for(int i=0;i<pop.size(); i++){
-			pop.get(i).getFitness().set(k*r.nextFloat());
+			pop.get(i).getFitness().reset(true);
+			// just not to receive warnings:
+			pop.get(i).getFitness().setValid(true);	
 		}
 	}
 }
